e805a3a5037b397d83577b729a046a441ebe5998
Daniel González <dani@Daniels-MacBook-Pro.local>
2025-11-12 19:15:18 -0300

    feat(PrimaveraProject): add last_schedule_date field to project attributes with reader and writer

--- src/main/java/org/mpxj/primavera/PrimaveraReader.java
+++ src/main/java/org/mpxj/primavera/PrimaveraReader.java
@@ -207,6 +207,7 @@ final class PrimaveraReader
          properties.setName(row.getString("proj_short_name")); // Temporary, updated later from the WBS
          properties.setDefaultTaskType(TaskTypeHelper.getInstanceFromXer(row.getString("def_duration_type")));
          properties.setStatusDate(row.getDate("last_recalc_date"));
+         properties.setLastScheduleDate(row.getDate("last_schedule_date"));
          properties.setFiscalYearStartMonth(row.getInteger("fy_start_month_num"));
          properties.setExportFlag(row.getBoolean("export_flag"));
          properties.setPlannedStart(row.getDate("plan_start_date"));
